8d5f2528fb86aab0141716b42b3c2d40707b02af
Nidheeshdas Thavorath <nidheeshdas@gmail.com>
2020-04-03 10:44:59 +0530

    added mathtype plugin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import MathType from "@wiris/ckeditor5-mathtype";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+          <h1>Hello CodeSandbox</h1>
+          <div className="App">
+            <h2>Using CKEditor 5 build in React</h2>
+            <CKEditor
+              editor={ClassicEditor}
+              config={{
+                plugins: [MathType],
+                toolbar: ["MathType", "ChemType"]
+              }}
+              data="<p>Hello from CKEditor 5!</p>"
+              onInit={editor => {
+                // You can store the "editor" and use when it is needed.
+                console.log("Editor is ready to use!", editor);
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
+              }}
+            />
+          </div>
+        </div>
+      );
 }
 
 export default App;
